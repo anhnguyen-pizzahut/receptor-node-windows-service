0eda2c031030c03d63e5cfe41a689f1c4fa7baec
Anh Nguyen <anh.nguyen@pizzahut.io>
2019-08-21 18:11:18 +0700

    task: add models for receptor

--- src/types.ts
+++ src/types.ts
@@ -20,10 +20,177 @@ export interface GmsOrderSyncPayload {
 }
 
 export interface GmsOrder {
-  orderId: string,
+  orderId: number,
   orderDetails: string,
 }
 
+export interface PaymentTypeInfo {
+  IdPaymentType?: number;
+  Name?: string;
+  ImageAddress?: string;
+  MayHaveChange: boolean | false;
+  Online: boolean | false;
+}
+
+export interface OrderPaymentInfo {
+  IntegrationCode?: string;
+  PaidValue: number | 0;
+  Online: boolean | false;
+  AuthorizationCode?: string;
+  PaymentType?: PaymentTypeInfo;
+}
+
+export interface OrderMenuDishOptionalItemInfo {
+  Name?: string;
+  Description?: string;
+  Quantity: number | 0;
+  TotalValue: number | 0;
+  TotalUnitValue: number | 0;
+  IntegrationCode?: string;
+}
+
+export interface OrderMenuDishOptionalInfo {
+  Name?: string;
+  Description?: string;
+  Value: number | 0;
+  IntegrationCode?: string;
+  ItemList: OrderMenuDishOptionalItemInfo[];
+}
+
+export interface OrderMenuDishFractionItemOptionalItem {
+  Name?: string;
+  Description?: string;
+  Quantity: number | 0;
+  TotalValue: number | 0;
+  TotalUnitValue: number | 0;
+  IntegrationCode?: string;
+}
+
+export interface OrderMenuDishFractionItemOptional {
+  Name: string;
+  Description: string;
+  Value: number | 0;
+  IntegrationCode?: string;
+  ItemList: OrderMenuDishFractionItemOptionalItem[];
+}
+
+export interface OrderMenuDishFractionItem {
+  Name?: string;
+  Description?: string;
+  Quantity: number | 0;
+  TotalValue: number | 0;
+  IntegrationCode?: string;
+  OptionalList: OrderMenuDishFractionItemOptional[];
+}
+
+export interface OrderMenuDishInfo {
+  Name?: string;
+  Description?: string;
+  Quantity: number | 0;
+  IsPizza: boolean | false;
+  IsOptionalsPopupActive: boolean | false;
+  TotalValue: number | 0;
+  TotalUnitValue: number | 0;
+  OriginalTotalValue: number | 0;
+  OriginalUnitValue: number | 0;
+  IntegrationCode?: string;
+  OptionalList?: OrderMenuDishOptionalInfo[];
+  FractionList?: OrderMenuDishFractionItem[];
+}
+
+export interface Coordinate {
+  Latitude?: number;
+  Longitude?: number;
+}
+
+export interface CustomerDeliveryAddress {
+  IdAddressType?: number;
+  Name?: string;
+  ZipCode?: string;
+  StateCode?: string;
+  City?: string;
+  Address?: string;
+  Neighborhood?: string;
+  Number?: string;
+  Complement?: string;
+  Reference?: string;
+  IdPhoneType?: number;
+  PhoneNumber?: string;
+  PhoneComplement?: string;
+  CoordinateData?: Coordinate;
+}
+
+export interface CustomerInfo {
+  Email?: string;
+  Name?: string;
+  Password?: string;
+  DocumentNumber?: string;
+  IdPhoneType?: number;
+  PhoneNumber?: string;
+  PhoneComplement?: string;
+  SendEmailNotification: boolean | false;
+  SendSMSNotification: boolean | false;
+  SecretQuestion?: string;
+  SecretAnswer?: string;
+  CallCenterUser: boolean | false;
+}
+
+export interface AddressInfo {
+  ZipCode?: string;
+  StateCode?: string;
+  City?: string;
+  Address?: string;
+  Neighborhood?: string;
+  Number?: string;
+  Complement?: string;
+  Reference?: string;
+  CoordinateData?: Coordinate;
+}
+
+export interface MenuGroupInfo {
+  IdMenuGroup?: number;
+  Name?: string;
+  Description?: string;
+}
+
+export interface RestaurantInfo {
+  IdRestaurant?: number;
+  Name?: string;
+  UniqueName?: string;
+  AcceptToGo: boolean | false;
+  Address?: AddressInfo;
+  PhotoAddress?: string;
+  MinimumOrderValue?: number;
+  MaximumOrderChange?: number;
+  ExternalName?: string;
+  ExternalLink?: string;
+  Online: boolean | false;
+  MenuGroups?: MenuGroupInfo[];
+  IdMenuGroupDefault?: number;
+  PreparationTime?: number;
+}
+
 export interface ReceptorOrder {
-  orderId: string,
+  PaymentList?: OrderPaymentInfo[];
+  MenuDishInfoList?: OrderMenuDishInfo;
+  CustomerDeliveryAddress: CustomerDeliveryAddress;
+  Observation?: string;
+  IntegrationCode?: string;
+  CustomerDocument?: string;
+  CustomerPhoneComplement?: string;
+  CustomerPhoneNumber?: string;
+  CustomerIdPhoneType?: number;
+  IdOrderStatus: number;
+  Total: number | 0;
+  DeliveryFee: number | 0;
+  Discount: number | 0;
+  ToGo: boolean | false;
+  ExptectedReadyDate?: Date;
+  FinalDate?: Date;
+  Date: Date;
+  Customer: CustomerInfo;
+  Restaurant: RestaurantInfo;
+  Key: number;
+  Subtotal: number | 0;
+  ReceiptDocument?: string;
 }
